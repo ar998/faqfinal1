7bfc864a3e71876e6de53b5827dbcc9d17e8e7c0
akanksha <ar998@njit.edu>
2018-05-07 17:26:18 -0400

    view changes

--- app/Profile.php
+++ app/Profile.php
@@ -8,10 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    protected $fillable = ['fname', 'lname', 'body', 'type'];
+    protected $fillable = ['fname', 'lname', 'body'];
 
 
     //
+    private $rules = array(
+        'fname' => 'required|alpha',
+        'lname' => 'required|alpha',
+        'body' => 'required|alpha|min:10',
+
+
+    );
+   // protected $fillable = ['fname', 'lname', 'body'];
+
     public function user()
     {
         return $this->belongsTo('App\User');
